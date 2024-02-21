310e8afceb8d22e45ae7228ca05ecea8892ea5b2
Hiren Rafaliya <hiren6997@gmail.com>
2024-02-21 14:13:11 +0530

    Initial Commit

---
+++ Hello SwiftUI/Hello_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_SwiftUIApp.swift
+//  Hello SwiftUI
+//
+//  Created by Hiren Rafaliya on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Hello_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
