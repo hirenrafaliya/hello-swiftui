4ef38efc597adbd7d103af706af5e9bbdca9730f
Hiren Rafaliya <hiren6997@gmail.com>
2024-03-06 15:33:19 +0530

    learn: gridstack

--- Hello SwiftUI/ContentView.swift
+++ Hello SwiftUI/ContentView.swift
@@ -7,25 +7,33 @@
 
 import SwiftUI
 
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let cols: Int
+    @ViewBuilder let content: (Int, Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<cols, id: \.self) { col in
+                        content(row, col)
+                    }
+                }
+            }
+        }
+    }
+}
+
 struct ContentView: View {
     @State private var amount = 0.0
     
     var body: some View {
-        NavigationView {
-            Form {
-                TextField("Enter your amount", text: Binding<String>(
-                    get: {
-                        "\(amount)"
-                    }, set: {
-                        amount = Double($0) ?? 0.0
-                    }
-                    
-                ))
-                
-            }
-        }
-        
-        
+        GridStack(rows: 20, cols: 10, content: { row, col in
+            Rectangle()
+                .fill(row % 2 == 0 ? col % 2 == 0 ? Color.orange : Color.blue : Color.green)
+                .frame(width: 20, height: 20)
+        })
     }
 }
 
